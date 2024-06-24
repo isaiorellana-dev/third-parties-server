f9b2f7cbe04a45dc8ebf57b961d6a19f1cb097ff
Isai <isaiorellana.dev@gmail.com>
2024-06-24 07:44:16 -0600

    Re-order html

--- dist/handlers/gmail.js
+++ dist/handlers/gmail.js
@@ -15,26 +15,21 @@ const fs = require("fs");
 const path = require("path");
 require("dotenv").config();
 const stringKey = process.env.DRIVE_KEY;
+const FROM = process.env.FROM;
 const key = JSON.parse(stringKey);
-// const auth = new google.auth.GoogleAuth({
-//   credentials: key,
-//   scopes: ["https://www.googleapis.com/auth/gmail.send"],
-// })
 const auth = new google_auth_library_1.JWT({
     email: key.client_email,
     key: key.private_key,
     scopes: ["https://www.googleapis.com/auth/gmail.send"],
-    subject: "customerservice@sweepstouch.com", // Correo de la cuenta desde la que se enviarán los correos
+    subject: FROM,
 });
 const sendEmail = (user, password, file, emails) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const gmail = googleapis_1.google.gmail({ version: "v1", auth: auth });
         const CCs = emails.slice(1);
         console.log(CCs);
-        // console.log("path en gmail", filePath)
-        // console.log(fs.readFileSync(filePath))
         const messageParts = [
-            'From: "Sweepstouch LLC" <customerservice@sweepstouch.com>',
+            `From: "Sweepstouch LLC" <${FROM}>`,
             `To: ${emails[0]}`,
             `Cc: ${CCs.join(", ")}`,
             "Subject: Test",
@@ -44,7 +39,7 @@ const sendEmail = (user, password, file, emails) => __awaiter(void 0, void 0, vo
             "--boundary",
             'Content-Type: text/html; charset="UTF-8"',
             "",
-            `<img src="cid:imagen1" style="width:100%"/><h1 style="color:#fc0680; font-family: Verdana, Geneva, Tahoma, sans-serif">ADV DE LA SEMANA</h1><div style="width: 100%;"><div style="width:fit-content; margin: 0 auto"><img src="cid:flyer" style="max-width: 500px" /></div></div><div style="margin-top:30px; font-family: Arial, Helvetica, sans-serif"><p>Para mas detalles de tu campaña, ingresa y dale click <a href="https://portal.sweepstouch.com/" style="color:#fc0680;">aquí.</a></p><p style="color:#fc0680; font-weight: bold">Estas son tus credenciales:</p><ul><li>User: ${user}</li><li>Password: ${password}</li></ul></div>`,
+            `<h1 style="color:#fc0680; font-family: Verdana, Geneva, Tahoma, sans-serif">ADV DE LA SEMANA</h1><div style="width: 100%;"><div style="width:fit-content; margin: 0 auto"><img src="cid:flyer" style="max-width: 500px" /></div></div><div style="margin-top:30px; font-family: Arial, Helvetica, sans-serif"><p>Para mas detalles de tu campaña, ingresa y dale click <a href="https://portal.sweepstouch.com/" style="color:#fc0680;">aquí.</a></p><p style="color:#fc0680; font-weight: bold">Estas son tus credenciales:</p><ul><li>User: ${user}</li><li>Password: ${password}</li></ul></div><img src="cid:imagen1" style="width:100%"/>`,
             "",
             "--boundary",
             "Content-Type: image/png",
--- src/handlers/gmail.ts
+++ src/handlers/gmail.ts
@@ -6,18 +6,14 @@ const path = require("path")
 require("dotenv").config()
 
 const stringKey = process.env.DRIVE_KEY!
+const FROM = process.env.FROM
 const key = JSON.parse(stringKey)
-
-// const auth = new google.auth.GoogleAuth({
-//   credentials: key,
-//   scopes: ["https://www.googleapis.com/auth/gmail.send"],
-// })
 
 const auth = new JWT({
   email: key.client_email,
   key: key.private_key,
   scopes: ["https://www.googleapis.com/auth/gmail.send"],
-  subject: "customerservice@sweepstouch.com", // Correo de la cuenta desde la que se enviarán los correos
+  subject: FROM,
 })
 
 const sendEmail = async (
@@ -31,11 +27,9 @@ const sendEmail = async (
 
     const CCs = emails.slice(1)
     console.log(CCs)
-    // console.log("path en gmail", filePath)
-    // console.log(fs.readFileSync(filePath))
 
     const messageParts = [
-      'From: "Sweepstouch LLC" <customerservice@sweepstouch.com>',
+      `From: "Sweepstouch LLC" <${FROM}>`,
       `To: ${emails[0]}`,
       `Cc: ${CCs.join(", ")}`,
       "Subject: Test",
@@ -45,7 +39,7 @@ const sendEmail = async (
       "--boundary",
       'Content-Type: text/html; charset="UTF-8"',
       "",
-      `<img src="cid:imagen1" style="width:100%"/><h1 style="color:#fc0680; font-family: Verdana, Geneva, Tahoma, sans-serif">ADV DE LA SEMANA</h1><div style="width: 100%;"><div style="width:fit-content; margin: 0 auto"><img src="cid:flyer" style="max-width: 500px" /></div></div><div style="margin-top:30px; font-family: Arial, Helvetica, sans-serif"><p>Para mas detalles de tu campaña, ingresa y dale click <a href="https://portal.sweepstouch.com/" style="color:#fc0680;">aquí.</a></p><p style="color:#fc0680; font-weight: bold">Estas son tus credenciales:</p><ul><li>User: ${user}</li><li>Password: ${password}</li></ul></div>`,
+      `<h1 style="color:#fc0680; font-family: Verdana, Geneva, Tahoma, sans-serif">ADV DE LA SEMANA</h1><div style="width: 100%;"><div style="width:fit-content; margin: 0 auto"><img src="cid:flyer" style="max-width: 500px" /></div></div><div style="margin-top:30px; font-family: Arial, Helvetica, sans-serif"><p>Para mas detalles de tu campaña, ingresa y dale click <a href="https://portal.sweepstouch.com/" style="color:#fc0680;">aquí.</a></p><p style="color:#fc0680; font-weight: bold">Estas son tus credenciales:</p><ul><li>User: ${user}</li><li>Password: ${password}</li></ul></div><img src="cid:imagen1" style="width:100%"/>`,
       "",
       "--boundary",
       "Content-Type: image/png",
